31610dcc3e4dc1380c19611ea2e20f4c5d3c652d
ffiric00 <filipfirich@gmail.com>
2022-03-15 18:16:38 +0100

    kreiranje

--- ffiric1.cpp
+++ ffiric1.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n"; 
+    std::cout << "NovoNovoNovoNovo!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
